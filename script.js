d309506599f39a10fb2015e7dc97c5c189b3cca2
saturn1809 <vyvipinyadav63@gmail.com>
2025-07-05 15:43:07 +0530

    Update

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ const menu = document.querySelector('.menu');
 
 start.addEventListener('click' , () => {
      menu.classList.toggle('hidden');
-    
+});
 
-})
+const lock_icon = document.querySelector('.lock-icon');
+const lock_bar = document.querySelector('.lock-bar');
 
-const lock_icon = document.querySelector('.lock-icon')
 lock_icon.addEventListener('click',()=>{
     lock_bar.classList.toggle('hidden');
     
-})
+});
 
-const lock_bar = document.querySelector('.lock-bar')
+document.addEventListener('click',(e)=>{
+    if (!menu.contains(e.target) && !start.contains(e.target)) {
+    menu.classList.add('hidden');
+  }
+
+  // Close lock bar if clicked outside
+  if (!lock_bar.contains(e.target) && !lock_icon.contains(e.target)) {
+    lock_bar.classList.add('hidden');
+  }
+
+});
+
+
 
 
 const arrow = document.querySelector('.arrow');
@@ -30,10 +42,18 @@ language.addEventListener('click' , () => {
 })
 
 const utils = document.querySelector('.utils');
-utils.addEventListener('click' , () => {
-    console.log('Clicked byme');
+const search_bar = document.querySelector('.settings-bar');
+utils.addEventListener('click' , (e) => {
+    //     e.stopPropagation();
+    search_bar.classList.toggle('hidden')
     
 })
+document.addEventListener('click',(e) =>{
+      if (!search_bar.contains(e.target) && !utils.contains(e.target)) {
+    search_bar.classList.add('hidden');
+  }
+});
+
 
 const dates = document.querySelector('.date');
 dates.addEventListener('click' , () => {
